941373fe418c27912af1136a5f1dda40d42fbcf7
Paulina Khew <paulinakhew12345@hotmail.com>
2020-06-11 14:39:07 -0400

    :sparkles: Add time, kills, deaths, and kd

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 def process_json_data(data):
     platforminfo = data['platformInfo']
+    stats = data['segments'][0]['stats']
 
     return_dict = {
         'avatar_url': platforminfo['avatarUrl'],
-        'username': platforminfo['platformUserHandle']
+        'username': platforminfo['platformUserHandle'],
+        'time_played': stats['timePlayed']['displayValue'],
+        'kills': stats['kills']['value'],
+        'deaths': stats['deaths']['value'],
+        'kd': round(stats['kd']['value'], 2)
     }
 
     return return_dict
