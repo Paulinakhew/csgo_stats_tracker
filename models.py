dfea376538c5c29a11936b3ebdc6e1b591d6551c
Paulina Khew <paulinakhew12345@hotmail.com>
2020-06-11 21:44:55 -0400

    :tada: Intial commit of all graphs

--- models.py
+++ models.py
@@ -8,7 +8,11 @@ def process_json_data(data):
         'time_played': stats['timePlayed']['displayValue'],
         'kills': stats['kills']['value'],
         'deaths': stats['deaths']['value'],
-        'kd': round(stats['kd']['value'], 2)
+        'kd': round(stats['kd']['value'], 2),
+        'wl': stats['wlPercentage']['value'],
+        'kd_percentile': stats['kd']['percentile'],
+        'kd_total': round(stats['kd']['value'], 2) + 1.00,
+        'kd_fraction': round(stats['kd']['value'] / (stats['kd']['value'] + 1.00), 2) * 100
     }
 
     return return_dict
