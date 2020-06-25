94db4958bb3bfa351a3e751b1235ed4c712207b8
Paulina Khew <paulinakhew12345@hotmail.com>
2020-06-24 22:55:33 -0400

    :sparkles: Add steam API call for all users

--- controller.py
+++ controller.py
@@ -22,11 +22,16 @@ def dashboard():
 @app.route('/<steam_id>')
 def user_dashboard(steam_id):
     resp = m.request_tracker_network_api(steam_id=steam_id)
+    steam_resp = m.request_steam_csgo_stats(steam_id=steam_id)
 
-    if resp.status_code == 200:
+    if resp.status_code == 200 and steam_resp.status_code == 200:
         data = resp.json()['data']
         return_dict = m.process_json_data(data=data)
-        return render_template('dashboard.html', data=return_dict)
+        return render_template(
+            'dashboard.html',
+            data=return_dict,
+            steam_data=steam_resp.json()
+        )
     else:
         return redirect('/')
 
