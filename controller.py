7fef44de6ca9d397da27b40512e82e379d92a8bc
Paulina Khew <paulinakhew12345@hotmail.com>
2020-06-29 21:48:22 -0400

    :zap: Get both steam api calls working and properly formatted

--- controller.py
+++ controller.py
@@ -12,10 +12,12 @@ def dashboard():
 @app.route('/<steam_id>')
 def user_dashboard(steam_id):
     resp = m.request_steam_csgo_stats(steam_id=steam_id)
+    user_info = m.request_steam_player_summary(steam_id=steam_id)
 
-    if resp.status_code == 200:
-        data = resp.json()
-        return_dict = m.process_json_data(data=data)
+    if resp.status_code == 200 and user_info.status_code == 200:
+        csgo_data = resp.json()
+        player_data = user_info.json()
+        return_dict = m.process_json_data(csgo_data=csgo_data, player_data=player_data)
         return render_template(
             'dashboard.html',
             data=return_dict,
--- models.py
+++ models.py
@@ -7,33 +7,37 @@ load_dotenv()
 STEAM_WEB_API_KEY = os.environ.get("STEAM_WEB_API_KEY")
 
 
-def process_json_data(data):
-    stats = data['playerstats']['stats']
+def process_json_data(csgo_data, player_data):
+    player_data = player_data['response']['players'][0]
+    stats = csgo_data['playerstats']['stats']
     kills = stats[0]['value']
     deaths = stats[1]['value']
+    kd = round(kills / deaths, 2)
     return_dict = {
-        'avatar_url': None,
-        'username': None,
+        'avatar_url': player_data['avatar'],
+        'username': player_data['personaname'],
         'time_played': stats[2]['value'],
         'kills': kills,
         'deaths': deaths,
-        'kd': round(kills / deaths, 2),
-        'kd_total': None,
-        'kd_fraction': None,
+        'kd': kd,
+        'kd_fraction': round(kd / (kd + 1.00), 2) * 100,
         'wins': stats[5]['value'],
-        'headshots': None,
-        'headshotpct': None,
-        'headshot_percentile': None,
-        'kd_percentile': None,
-        'wl_percentage': None,
-        'wl_percentile': None
+        'headshots': stats[23]['value'],
     }
     return return_dict
 
 
-def request_steam_csgo_stats(steam_id=76561199056418213):
+def request_steam_csgo_stats(steam_id):
     resp = requests.get(
         url=f'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key={STEAM_WEB_API_KEY}&steamid={steam_id}'
+    )
+
+    return resp
+
+
+def request_steam_player_summary(steam_id):
+    resp = requests.get(
+        url=f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_WEB_API_KEY}&steamids={steam_id}'
     )
 
     return resp
